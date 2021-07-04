8bc3af1baa205b060a20ce5c09441171962e5380
wangshijun <wangshijun2010@gmail.com>
2021-07-04 09:59:00 +0800

    chore: bump deps to latest

--- server/libs/auth.js
+++ server/libs/auth.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const Mcrypto = require('@ocap/mcrypto');
+const WalletAuthenticator = require('@blocklet/sdk/lib/wallet-authenticator');
 const AuthService = require('@blocklet/sdk/service/auth');
 const AuthNedbStorage = require('@arcblock/did-auth-storage-nedb');
 const { fromSecretKey, WalletType } = require('@ocap/wallet');
-const { WalletHandlers, WalletAuthenticator } = require('@arcblock/did-auth');
+const { WalletHandlers } = require('@arcblock/did-auth');
 
 const env = require('./env');
 const logger = require('./logger');
@@ -11,20 +12,7 @@ const logger = require('./logger');
 const type = WalletType({ role: Mcrypto.types.RoleType.ROLE_APPLICATION });
 const wallet = fromSecretKey(process.env.APP_SK || process.env.BLOCKLET_APP_SK, type).toJSON();
 
-const icon = `${env.serverUrl}/logo.png`;
-const walletAuth = new WalletAuthenticator({
-  wallet,
-  appInfo: ({ baseUrl }) => ({
-    name: env.appName,
-    description: env.appDescription,
-    icon: env.appIcon || icon,
-    link: baseUrl,
-  }),
-  chainInfo: {
-    host: env.chainHost,
-    id: 'demo',
-  },
-});
+const walletAuth = new WalletAuthenticator();
 
 const dbOnload = (err, dbName) => {
   if (err) {
