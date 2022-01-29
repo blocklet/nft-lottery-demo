7b35de8fb8c517a324dd1cde98aafaa3429f7b68
zhanghan <Lancelot_Lewis@163.com>
2022-01-29 16:28:14 +0800

    chore: change `blocklet publish` -> `blocklet upload`

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   base: process.env.BASE_URL,
   plugins: [react({ removeDevtoolsInProd: true, injectReact: true })],
   server: {
+    hmr: {
+      port: 443,
+    },
     port: process.env.PORT || process.env.BLOCKLET_PORT,
+    strictPort: true,
     proxy: {
       '/api': 'http://localhost:3030',
     },
